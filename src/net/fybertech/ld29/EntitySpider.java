e574cb08764e53e4e378d7f441e2a892a550193c
FyberOptic <fyberoptic@gmail.com>
2014-05-05 13:03:00 -0400

    Spider Animation

--- src/net/fybertech/ld29/EntitySpider.java
+++ src/net/fybertech/ld29/EntitySpider.java
@@ -3,9 +3,24 @@ package net.fybertech.ld29;
 public class EntitySpider extends Entity 
 {
 
+	int frame = 0;
+	int frameCount = 0;
+	int baseTile = (3 * 32) + 5;
+	
 	public EntitySpider()
 	{
-		tileNum = (3 * 32) + 5;
+		tileNum = baseTile;
+	}
+	
+	
+	public void tick()
+	{
+		frameCount++;
+		if (frameCount > 1) { frame++; frameCount = 0; } 
+		
+		if (frame >= 3) frame = 0;
+		
+		tileNum = baseTile + frame;
 	}
 	
 }
