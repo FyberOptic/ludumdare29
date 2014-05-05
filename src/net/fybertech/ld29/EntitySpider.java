5f05bb0c9b142d7468e16bc52b89a097dcf3c499
FyberOptic <fyberoptic@gmail.com>
2014-05-05 14:54:30 -0400

    Texture tweaks, basic spider AI

--- src/net/fybertech/ld29/EntitySpider.java
+++ src/net/fybertech/ld29/EntitySpider.java
@@ -10,15 +10,32 @@ public class EntitySpider extends Entity
 	public EntitySpider()
 	{
 		tileNum = baseTile;
+		xVel = 12;
+		height = 9;
 	}
 	
 	
+	@Override
+	public void update(float deltaTime)
+	{
+		float delta = deltaTime / 1000.0f;
+		yVel += 300 * delta;
+		if (yVel < -100) yVel = -100;
+		if (yVel > 400) yVel = 400;
+		
+		super.update(deltaTime);
+		
+		if (this.xCollide) xVel = -xVel; 
+	}
+	
+	@Override
 	public void tick()
 	{
 		frameCount++;
-		if (frameCount > 1) { frame++; frameCount = 0; } 
+		if (frameCount > 2) { if (xVel >= 0) frame++; else frame--; frameCount = 0; } 
 		
 		if (frame >= 3) frame = 0;
+		if (frame < 0) frame = 2;
 		
 		tileNum = baseTile + frame;
 	}
